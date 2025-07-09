4a5d1eb2f6af1fa8e4d1032a823798239695f702
TimilehinOlajolo <olajolotaiwo2@gmail.com>
2025-07-09 20:22:15 +0100

    initial commit

---
+++ W3School/06-operators.js
@@ -0,0 +1,16 @@
+/*
+= (assignment operator)
+used to assign a variable
+
++ (Addition Operator)
+used to add numbers
+
+* (multiplication operator)
+used to multiply numbers 
+*/
+
+//TYPES OF OPERATIONS
+/* 
+ARITHMETIC OPERATORS 
+used to perform arithmetic on number
+*/
